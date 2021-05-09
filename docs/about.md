47088ad2c290c0ee0597aa9c87c14562cb4b8584
kcf-jackson <kcf.jackson@gmail.com>
2021-05-10 00:46:39 +1000

    Include missing files

--- docs/about.md
+++ docs/about.md
@@ -1,11 +1,42 @@
 ---
-title: The Project
+title: About
 ---
+
+
+## The Project
 
 This project is funded by the [R consortium](https://www.r-consortium.org/).
 
 
-## Ongoing investigations
+<!---
+### Some ongoing ideas / work
 
-- creating UIs for live spatial analysis using the scriptable web environment of **sketch**
-- transpiling to WASM via AssemblyScript
+- Create new graphics device to support real-time plotting
+- Create UIs for live spatial analysis
+- Support transpiling to WASM via AssemblyScript
+- Continue improving documentation, tutorial, showcase and testing
+-->
+
+
+---
+
+## The Team
+
+### Jackson Kwok @ SVI
+
+### Kate Saunders @ QUT
+
+
+
+---
+
+## The Website
+
+This website is built with [Docusaurus](https://docusaurus.io/).
+
+
+
+
+
+
+<link href="../css/docs-styles.css" rel="stylesheet"></link>
--- src_R_Markdown/makefile.R
+++ src_R_Markdown/makefile.R
@@ -1,7 +1,8 @@
 library(magrittr)
 library(purrr)
 
-# Helper functions
+
+# Helper functions --------------------------------------------------------
 remove_first_line <- function(fpath) {
     lines <- readLines(fpath)
     if (lines[1] == "") {
@@ -19,6 +20,7 @@ remove_html_preserve <- function(fpath) {
 }
 
 highlight_codeblock <- function(fpath, block_num, line_num) {
+    if (length(fpath) == 0) return(NULL)
     if (length(block_num) != length(line_num)) {
         stop("The `block_num` argument must match the `line_num` argument.")
     }
@@ -35,33 +37,82 @@ highlight_codeblock <- function(fpath, block_num, line_num) {
 clean_md <- compose(remove_html_preserve, remove_first_line)
 
 
+# Compute and save hash of all the files in the current directory
+update_hash <- function(file = "file_hash.csv") {
+    file_hash <- tools::md5sum(list_files(recursive = T))
+    write.csv(data.frame(file_hash), file = file)
+}
+
+# Filter a list of files by previous hash stored in a file
+filter_by_hash <- function(fs, file = "file_hash.csv") {
+    df0 <- read.csv(file) %>%
+        set_colnames(c("file_path", "file_hash"))
+
+    new_hash <- tools::md5sum(fs)
+    df1 <- data.frame(file_path = names(new_hash),
+                      file_hash = as.character(new_hash))
+
+    replace_na <- function(x, replacement) {
+        x[is.na(x)] <- replacement
+        x
+    }
+    join_df <- df1 %>%
+        dplyr::left_join(df0, by = "file_path") %>%
+        replace_na(replacement = "") %>%
+        dplyr::filter(file_hash.x != file_hash.y)
+    join_df$file_path
+}
+
+
 # Point-free declaration
 echo <- . %T>% message()
 pipe <- compose %>% partial(.dir = "forward")
 list_files <- list.files %>% partial(full.names = T)
 quiet_render <- rmarkdown::render %>% partial(quiet = T) %>% quietly()
+guard <- function(f) {
+    function(x) {
+        if (length(x) == 0) return(NULL)
+        f(x)
+    }
+}
+
+
+# Main --------------------------------------------------------------------
+# Create file hash
+if (!file.exists("file_hash.csv")) {
+    filter_by_hash <- identity
+}
 
 
 # Processing directory
 message("Rendering Rmd files to md files...")
-pipe_render <- pipe(echo, quiet_render)
-list_files("./published/tutorial", "*[.]Rmd") %>% walk(pipe_render)
-list_files("./published/docs", "*[.]Rmd") %>% walk(pipe_render)
+pipe_render <- pipe(echo, guard(quiet_render))
+list_files("./published/tutorial", "*[.]Rmd") %>%
+    filter_by_hash() %>%
+    walk(pipe_render)
+list_files("./published/docs", "*[.]Rmd") %>%
+    filter_by_hash() %>%
+    walk(pipe_render)
 
 
 message("Cleaning md files")
-pipe_clean_md <- pipe(echo, clean_md)
-list_files("./published/tutorial", "*[.]md") %>% walk(pipe_clean_md)
-list_files("./published/docs", "*[.]md") %>% walk(pipe_clean_md)
-
+pipe_clean_md <- pipe(echo, guard(clean_md))
+list_files("./published/tutorial", "*[.]md") %>%
+    filter_by_hash() %>%
+    walk(pipe_clean_md)
+list_files("./published/docs", "*[.]md") %>%
+    filter_by_hash() %>%
+    walk(pipe_clean_md)
 
 
 # Processing file - Code highlighting
 message("Adding line highlight to md files...")
-"./published/tutorial/creative_coding.md" %T>%
+"./published/tutorial/creative_coding.md" %>%
+    filter_by_hash() %T>%
     message() %>%
     highlight_codeblock(2:3, list(17:24, c(13, 27, 31, 34:37)))
-"./published/docs/getting-started.md" %T>%
+"./published/docs/getting-started.md" %>%
+    filter_by_hash() %T>%
     message() %>%
     highlight_codeblock(2:3, list(c(3:10, 18:21), c(6, 9:28, 43)))
 
@@ -72,26 +123,35 @@ message("All processing completed.")
 # Copying / Deployment
 # message("Copying docs to website...")
 list_files("./published/docs", "*[.]md") %>%
+    filter_by_hash() %>%
     walk(~.x %T>% message() %>% file.copy(to = "../docs/", overwrite = T))
 
 message("Copying tutorial to website...")
 list_files("./published/tutorial", "*[.]md") %>%
+    filter_by_hash() %>%
     walk(~.x %T>% message() %>% file.copy(to = "../tutorial/", overwrite = T))
 
 message("Copying assets to website...")
 list_files("./published/css") %>%
+    filter_by_hash() %>%
     walk(~.x %T>% message() %>% file.copy(to = "../static/css/", overwrite = T))
 list_files("./published/img") %>%
+    filter_by_hash() %>%
     walk(~.x %T>% message() %>% file.copy(to = "../static/img/", overwrite = T))
-"./published/html/index.html" %T>%
+"./published/html/index.html" %>%
+    filter_by_hash() %T>%
     message() %>%
     file.copy(to = "../static/html/logo.html", overwrite = T)
-
 
 
 # Cleaning up
 message("Cleaning up HTML files")
 list_files("./published/docs", "*[.]html") %>%
+    filter_by_hash() %>%
     walk(pipe(echo, file.remove))
 list_files("./published/tutorial", "*[.]html") %>%
+    filter_by_hash() %>%
     walk(pipe(echo, file.remove))
+
+# Update the hash of all files
+update_hash("file_hash.csv")
